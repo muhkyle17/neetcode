b2b9308f00880099e69612544481d44328b03aab
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-09-18 14:00:38 -0700

    A cleaner way of doing it

--- hasDuplicate.js
+++ hasDuplicate.js
@@ -16,3 +16,21 @@ class Solution {
     return boolean
   }
 }
+
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums) {
+    const numsSet = new Set()
+    for (const num of nums) {
+      if (numsSet.has(num)) return true
+
+      numsSet.add(num)
+    }
+    console.log(numsSet, 'numSet')
+
+    return false
+  }
+}
