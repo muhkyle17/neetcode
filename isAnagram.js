36d4c1be2557f6faba110248169110606f548659
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-09-23 16:05:00 -0700

    Add another solution using bracket notation and hashmap

--- isAnagram.js
+++ isAnagram.js
@@ -12,6 +12,43 @@ class Solution {
   }
 }
 
-const solution = new Solution()
-console.log(solution.isAnagram('racecar', 'carrace'))
-console.log(solution.isAnagram('jar', 'jam'))
+// const solution = new Solution()
+// console.log(solution.isAnagram('racecar', 'carrace'))
+// console.log(solution.isAnagram('jar', 'jam'))
+
+class Solution2 {
+  /**
+   * @param {string} s
+   * @param {string} t
+   * @return {boolean}
+   */
+  isAnagram(s, t) {
+    if (s.length !== t.length) {
+      return false
+    }
+
+    const countS = {}
+    const countT = {}
+
+    for (let i = 0; i < s.length; i++) {
+      // countS[s[i]] = 1 + (countS[s[i]] || 0)
+      countS[s[i]] = 'hi'
+      countT[t[i]] = 1 + (countT[t[i]] || 0)
+    }
+
+    console.log(countS, 'countS')
+    console.log(countT, 'countT')
+
+    for (const key in countS) {
+      if (countS[key] !== countT[key]) {
+        return false
+      }
+    }
+
+    return true
+  }
+}
+
+const solution2 = new Solution2()
+console.log(solution2.isAnagram('racecar', 'carrace'))
+console.log(solution2.isAnagram('jar', 'jam'))
