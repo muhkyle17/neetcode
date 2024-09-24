143696dcf692078ddf551f924fcf32833af4f1f4
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-09-23 21:27:39 -0700

    Add solution from neetcode

--- twoIntegerSum.js
+++ twoIntegerSum.js
@@ -24,3 +24,33 @@ class Solution {
 const solution = new Solution()
 const result = solution.twoSum([4, 5, 6], 10)
 console.log(result, 'result')
+
+class Solution2 {
+  /**
+   * @param {number[]} nums
+   * @param {number} target
+   * @return {number[]}
+   */
+  twoSum(nums, target) {
+    const map = new Map()
+
+    for (let index = 0; index < nums.length; index++) {
+      const num = nums[index]
+      const complement = target - num
+      const sumIndex = map.get(complement)
+
+      const isTarget = map.has(complement)
+      if (isTarget) {
+        return [index, sumIndex]
+      }
+
+      map.set(num, index)
+    }
+
+    return [-1, -1]
+  }
+}
+
+const solution2 = new Solution2()
+const result2 = solution2.twoSum([4, 5, 6], 10)
+console.log(result2, 'result2')
