ac27fedc87a139b85acf503d2b9f90bbd543a7b3
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-09-23 17:24:27 -0700

    Return correct solution

--- twoIntegerSum.js
+++ twoIntegerSum.js
@@ -7,22 +7,20 @@ class Solution {
   twoSum(nums, target) {
     const targetNums = {}
 
-    // targetNums[0] = nums[0]
     for (let i = 0; i < nums.length; i++) {
-      for (let j = 1; j < nums.length; j++) {
-        if (nums[i] + nums[j] === target) {
-          console.log(`It is nums${i} + nums${j}`)
-          // targetNums[i] = nums[i]
-          // targetNums[j] = nums[j]
-        }
+      const complement = target - nums[i]
+
+      if (complement in targetNums) {
+        return [targetNums[complement], i]
       }
+
+      targetNums[nums[i]] = i
     }
 
-    return targetNums
+    return []
   }
 }
 
 const solution = new Solution()
-// const sum = solution.twoSum([3,4,5,6], 7)
-const sum = solution.twoSum([4, 5, 6], 10)
-console.log(sum, 'sum')
+const result = solution.twoSum([4, 5, 6], 10)
+console.log(result, 'result')
