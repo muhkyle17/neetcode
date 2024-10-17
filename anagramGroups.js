e0594c07573a28c6b2c5968468f6fbeeb6739e80
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-17 16:58:51 -0700

    Add solution from neetcode

--- anagramGroups.js
+++ anagramGroups.js
@@ -26,3 +26,30 @@ class Solution {
 
 // const solution = new Solution()
 // console.log(solution.groupAnagrams(['act', 'pots', 'tops', 'cat', 'stop', 'hat']), 'output')
+
+class Solution2 {
+  /**
+   * @param {string[]} strs
+   * @return {string[][]}
+   */
+
+  groupAnagrams(strs) {
+    const res = {}
+    for (let str of strs) {
+      const count = new Array(26).fill(0)
+      for (let char of str) {
+        count[char.charCodeAt(0) - 'a'.charCodeAt(0)] += 1
+      }
+
+      const key = count.join(',')
+      if (!res[key]) {
+        res[key] = []
+      }
+      res[key].push(str)
+    }
+    return Object.values(res)
+  }
+}
+
+const solution2 = new Solution2()
+console.log(solution2.groupAnagrams(['act', 'pots', 'tops', 'cat', 'stop', 'hat']))
